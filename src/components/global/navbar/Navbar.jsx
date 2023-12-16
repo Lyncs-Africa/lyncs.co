36212a111f20e2195182db70841bb6aab2cd3370
Adam Jamiu <adamjamiu98@gmail.com>
2023-12-16 07:20:26 +0100

    new API key

--- src/components/global/navbar/Navbar.jsx
+++ src/components/global/navbar/Navbar.jsx
@@ -23,19 +23,15 @@ function Navbar(mode) {
 
   function useOutsideAlerter(ref) {
     useEffect(() => {
-      /**
-       * Alert if clicked on outside of element
-       */
+
       function handleClickOutside(event) {
         if (ref.current && !ref.current.contains(event.target)) {
           setToggle(false)
         }
       }
 
-      // Bind the event listener
       document.addEventListener("mousedown", handleClickOutside);
       return () => {
-        // Unbind the event listener on clean up
         document.removeEventListener("mousedown", handleClickOutside);
       };
     }, [ref]);
@@ -87,7 +83,6 @@ function Navbar(mode) {
                     alt="Lyncs Logo"
                   />
                 </>
-
               }
             </Link>
 
