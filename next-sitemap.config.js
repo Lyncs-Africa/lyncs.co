d027f5b1325bc240c7fef1ae7567e41beca567ae
blackmanseun <sulemanseun@gmail.com>
2024-10-21 17:30:10 +0100

    Updated the sitemap of the website

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.lyncs.africa/', // Replace with your website URL
+  generateRobotsTxt: true, // (optional)
+  changefreq: 'daily',
+  priority: 0.7,
+};
