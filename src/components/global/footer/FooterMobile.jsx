5a2762c603859a1de04f3ee8a4783a88df4084ae
blackmanseun <sulemanseun@gmail.com>
2025-03-04 13:46:54 +0100

    Updated the mobile footer

--- src/components/global/footer/FooterMobile.jsx
+++ src/components/global/footer/FooterMobile.jsx
@@ -35,7 +35,7 @@ function LyncsFooterMobile() {
   let year = date.getFullYear()
   return (
     <>
-      <footer className="p-4 bg-[#F1F1F8] sm:p-6 dark:bg-gray-900 px-4 md:hidden font-poppins">
+      <footer className="p-4 sm:p-6 bg-gray-900 px-4 md:hidden font-poppins">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href="#" className="flex items-center">
@@ -226,9 +226,9 @@ function LyncsFooterMobile() {
           {/*  </div>*/}
           {/*</div>*/}
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="text-sm sm:text-center text-gray-400">
             © {year}{" "}
             <a href="#" className="hover:underline">
               Lyncs™
